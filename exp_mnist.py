e1884da1ed02e8784c2347a1e97040f510344bc3
dhlee <dhlee@dhlee-Deepnet.(none)>
2015-02-19 16:51:46 -0500

    dhlee

--- exp_mnist.py
+++ exp_mnist.py
@@ -5,7 +5,7 @@ from util import *
 datapath = '/data/lisa/data/mnist/mnist.pkl'
 (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = np.load(datapath)
 
-init_W, init_b = ParamInit('randn',0,0.01), ParamInit('zeros')
+init_W, init_b = ParamInit('randn'), ParamInit('zeros')
 
 X = DesignMatrixDataLayer('X', train_x, 100)
 Y = DesignMatrixDataLayer('Y', one_hot(train_y), 100)
@@ -24,7 +24,7 @@ l2 = FullyConnectedLayer(name='h2',
                          init_W=init_W,
                          init_b=init_b)
 
-net = SeqNet('net', X, l1, l2)
+net = SeqNet('net', X, l1,    l2)
 cost = NLL_mul(net.fprop(), Y.fprop())
 ipdb.set_trace()
 
