dd3b26cd09efd853ecc55ebba396f4e211d1ade9
Junyoung <elecegg@gmail.com>
2015-02-20 02:49:53 -0500

    removed

--- net.py
+++ net.py
@@ -19,43 +19,8 @@ class Net(Layer):
         self.nodes = nodes
         self.edges = edges
 
-<<<<<<< HEAD
-class FeedForwardNet(Net):
-    def __init__(self, name, *layers):
-        self.name = name
-        self.layers = layers
-
-    @property
-    def params(self):
-        return flatten([layer.params for layer in self.layers])
-
-    def fprop(self, x=None):
-        for layer in self.layers:
-            x = layer.fprop(x)
-        return x
-
-
-class FullyConnectedLayer(Layer):
-    # Implementations of Layer
-    def __init__(self,
-                 name,
-                 n_in,
-                 n_out,
-                 unit='relu',
-                 init_W=ParamInit('randn'),
-                 init_b=ParamInit('zeros')):
-        self.__dict__.update(locals())
-        del self.self
-        self.W = init_W.get(n_in, n_out)
-        self.b = init_b.get(n_out)
-
-    @property
-    def params(self):
-        return [self.W, self.b]
-=======
     def get_params(self):
         return flatten([node.get_params() for node in self.nodes])
->>>>>>> 6ca993fc6736323ec3e8bbd6abdbd96dc4d03a8a
 
     def fprop(self, x):
         ipdb.set_trace()
