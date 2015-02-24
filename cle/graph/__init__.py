73e08781eadbc61b98eca918b22a23361dd81b8e
Junyoung <you@example.com>
2015-02-23 23:20:17 -0500

    working version

---
+++ cle/graph/__init__.py
@@ -0,0 +1,20 @@
+import theano
+import theano.tensor as T
+
+from cle.cle.util import PickleMixin, OrderedDict, tolist
+
+
+class TheanoMixin(object):
+    """
+    WRITEME
+
+    Parameters
+    ----------
+    .. todo::
+    """
+    def build_theano_graph(self, inputs, outputs, updates=[]):
+        return theano.function(inputs=inputs,
+                               outputs=outputs,
+                               updates=updates,
+                               on_unused_input='ignore',
+                               allow_input_downcast=True)
