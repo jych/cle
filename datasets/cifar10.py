7163a8bb01d862aa6398d539d1fd5675239e0982
Junyoung <you@example.com>
2015-03-01 18:26:14 -0500

    added cifar10 dataset

---
+++ datasets/cifar10.py
@@ -0,0 +1,49 @@
+import ipdb
+import numpy as np
+import theano.tensor as T
+from cle.cle.data import DesignMatrix
+
+
+class CIFAR10(DesignMatrix):
+    """
+    CIFAR10 batch provider
+
+    Parameters
+    ----------
+    .. todo::
+    """
+    def __init__(self, **kwargs):
+        super(CIFAR10, self).__init__(**kwargs)
+        self.data = self.load_data()
+        self.ndata = self.num_examples()
+        if self.batch_size is None:
+            self.batch_size = self.ndata
+        self.nbatch = int(np.ceil(self.ndata / float(self.batch_size)))
+        self.index = -1
+
+    def load_data(self):
+        X = np.load(self.path[0])
+        y = np.load(self.path[1])
+        return (X, y)
+
+    def num_examples(self):
+        return self.data[0].shape[0]
+
+    def batch(self, data, i):
+        size = self.batch_size
+        ndata = self.ndata
+        return data[i*size:min((i+1)*size, ndata)]
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.index += 1
+        if self.index < self.nbatch:
+            return (self.batch(data, self.index) for data in self.data)
+        else:
+            self.index = -1
+            raise StopIteration()
+
+    def theano_vars(self):
+        return [T.fmatrix('x'), T.fmatrix('y')]
