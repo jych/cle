b2a23b69e4c9debb293c1bb5ed19efb54b4e054a
Junyoung <you@example.com>
2015-03-01 18:16:13 -0500

    added cifar10 & conv tutorial

--- tutorials/cifar10.py
+++ tutorials/cifar10.py
@@ -107,14 +107,3 @@ mainloop = Training(
     extension=extension
 )
 mainloop.run()
-
-# What are not done yet
-# 1. Monitoring                      done!
-# 2. Serialization / Checkpoint      done! Thanks to kastnerkyle and Blocks
-#                                    working on early stopping
-# 3. Dropout: use Theano.clone
-# 4. Other Regularization
-# 5. RNN                             done!
-# 6. CNN                             donghyunlee is doing
-# 7. VAE                             laurent-dinh????????? :)
-# 8. Predefined nets: larger building block such as MLP, ConvNet and Stacked RNN
