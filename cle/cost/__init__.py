8c5f7b1d42d11a62e14844df98f9fe1079284ec9
Junyoung <you@example.com>
2015-03-06 02:17:48 -0500

    fixed bug

--- cle/cost/__init__.py
+++ cle/cost/__init__.py
@@ -52,9 +52,9 @@ def Gaussian(y, mu, logvar):
     mu     : FullyConnected (Linear)
     logvar : FullyConnected (Linear)
     """
-    #nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar, axis=:1)
+    #nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar, axis=1)
     nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
-                      T.log(2 * np.pi), axis=-1)
+                      T.log(2 * np.pi), axis=1)
     return nll
 
 
@@ -76,9 +76,8 @@ def GMM(y, mu, logvar, coeff):
     logvar = logvar.reshape((logvar.shape[0],
                              logvar.shape[1] / coeff.shape[-1],
                              coeff.shape[-1]))
-    #nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar, axis=-1)
-    ipdb.set_trace()
+    #nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar, axis=1)
     nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
-                      T.log(2 * np.pi), axis=-1)
-    nll = logsumexp(T.log(coeff) + nll, axis=-1)
+                      T.log(2 * np.pi), axis=1)
+    nll = logsumexp(T.log(coeff) + nll, axis=1)
     return nll
