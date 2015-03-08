cb5b6daa5fc7504c042a350906cd4d0936ef944d
Junyoung <you@example.com>
2015-03-07 22:05:37 -0500

    removing numerical instabilities

--- cle/cost/__init__.py
+++ cle/cost/__init__.py
@@ -52,7 +52,8 @@ def Gaussian(y, mu, logvar):
     mu     : FullyConnected (Linear)
     logvar : FullyConnected (Linear)
     """
-    logvar = T.maximum(logvar, np.log(tol))  # added tolerance value
+    #logvar = T.maximum(logvar, np.log(tol))  # added tolerance value
+    logvar = T.log(T.exp(logvar) + tol)
     nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
                       T.log(2 * np.pi), axis=1)
     return nll
@@ -76,7 +77,8 @@ def GMM(y, mu, logvar, coeff, tol=0.):
     logvar = logvar.reshape((logvar.shape[0],
                              logvar.shape[1] / coeff.shape[-1],
                              coeff.shape[-1]))
-    logvar = T.maximum(logvar, np.log(tol))  # added tolerance value
+    #logvar = T.maximum(logvar, np.log(tol))  # added tolerance value
+    logvar = T.log(T.exp(logvar) + tol)
     nll = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
                       T.log(2 * np.pi), axis=1)
     nll = logsumexp(T.log(coeff) + nll, axis=1)
