94920dfcf92229d7916f6c88997f735f37208cfe
Junyoung <you@example.com>
2015-03-14 01:49:59 -0400

    fix in GMM cost

--- cle/cost/__init__.py
+++ cle/cost/__init__.py
@@ -82,7 +82,7 @@ def GMM(y, mu, logvar, coeff, tol=0.):
                       T.log(2 * np.pi), axis=1)
     nll = logsumexp(T.log(coeff) + nll, axis=1)
     """
-    inner = 0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
+    inner = -0.5 * T.sum(T.sqr(y - mu) * T.exp(-logvar) + logvar +
                         T.log(2 * np.pi), axis=1)
     nll = -logsumexp(T.log(coeff) + inner, axis=1)
    
