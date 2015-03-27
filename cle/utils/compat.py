af91462060b89d8792932071d45bec99b9011bb2
Junyoung <you@example.com>
2015-03-26 22:27:37 -0400

    added compat

---
+++ cle/utils/compat.py
@@ -0,0 +1,32 @@
+""" Compatibility layer
+"""
+from theano.compat import six
+
+
+__all__ = ('OrderedDict,')
+
+
+if six.PY3:
+    from collections import OrderedDict
+else:
+    from theano.compat import OrderedDict
+
+
+def first_key(obj):
+    """ Return the first key
+
+    Parameters
+    ----------
+    obj: dict-like object
+    """
+    return six.next(six.iterkey(obj))
+
+
+def first_value(obj):
+    """ Return the first value
+
+    Parameters
+    ----------
+    obj: dict-like object
+    """
+    return six.next(six.itervalues(obj))
