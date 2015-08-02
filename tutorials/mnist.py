866a56baa35851f7354b57b66b584f7deefd25e1
Junyoung <elecegg@gmail.com>
2015-08-02 02:09:48 -0400

    updated mnist demo

--- tutorials/mnist.py
+++ tutorials/mnist.py
@@ -1,8 +1,8 @@
 import ipdb
 import numpy as np
 
+from cle.cle.cost import NllMulInd
 from cle.cle.data import Iterator
-from cle.cle.graph.net import Net
 from cle.cle.models import Model
 from cle.cle.layers import InitCell, OnehotLayer
 from cle.cle.layers.cost import MulCrossEntropyLayer
@@ -16,15 +16,15 @@ from cle.cle.train.ext import (
     EarlyStopping
 )
 from cle.cle.train.opt import RMSProp
-from cle.cle.utils import error, predict, OrderedDict
+from cle.cle.utils import error, flatten, predict, OrderedDict
 from cle.datasets.mnist import MNIST
 
 
 # Set your dataset
-#data_path = '/data/lisa/data/mnist/mnist.pkl'
-#save_path = '/u/chungjun/repos/cle/saved/'
-data_path = '/home/junyoung/data/mnist/mnist.pkl'
-save_path = '/home/junyoung/repos/cle/saved/'
+data_path = '/data/lisa/data/mnist/mnist.pkl'
+save_path = '/u/chungjun/repos/cle/saved/'
+#data_path = '/home/junyoung/data/mnist/mnist.pkl'
+#save_path = '/home/junyoung/repos/cle/saved/'
 
 batch_size = 128
 debug = 0
@@ -40,8 +40,7 @@ init_W = InitCell('randn')
 init_b = InitCell('zeros')
 
 # Define nodes: objects
-model.inputs = trdata.theano_vars()
-x, y = model.inputs
+x, y = trdata.theano_vars()
 # You must use THEANO_FLAGS="compute_test_value=raise" python -m ipdb
 if debug:
     x.tag.test_value = np.zeros((batch_size, 784), dtype=np.float32)
@@ -50,39 +49,45 @@ if debug:
 inputs = [x, y]
 inputs_dim = {'x':784, 'y':1}
 
-onehot = OnehotLayer(name='onehot',
-                     parent=['y'],
-                     nout=10)
-
 h1 = FullyConnectedLayer(name='h1',
                          parent=['x'],
+                         parent_dim=[784],
                          nout=1000,
                          unit='relu',
                          init_W=init_W,
                          init_b=init_b)
 
-h2 = FullyConnectedLayer(name='h2',
-                         parent=['h1'],
-                         nout=10,
-                         unit='softmax',
-                         init_W=init_W,
-                         init_b=init_b)
+output = FullyConnectedLayer(name='output',
+                             parent=['h1'],
+                             parent_dim=[1000],
+                             nout=10,
+                             unit='softmax',
+                             init_W=init_W,
+                             init_b=init_b)
 
-cost = MulCrossEntropyLayer(name='cost', parent=['onehot', 'h2'])
+# You will fill in a list of nodes
+nodes = [h1, output]
 
-# You will fill in a list of nodes and fed them to the model constructor
-nodes = [onehot, h1, h2, cost]
+# Initalize the nodes
+for node in nodes:
+    node.initialize()
 
-# Your model will build the Theano computational graph
-mlp = Net(inputs=inputs, inputs_dim=inputs_dim, nodes=nodes)
-mlp.build_graph()
+params = flatten([node.get_params().values() for node in nodes])
 
-# You can access any output of a node by doing model.nodes[$node_name].out
-cost = mlp.nodes['cost'].out
-err = error(predict(mlp.nodes['h2'].out), predict(mlp.nodes['onehot'].out))
-cost.name = 'cost'
+# Build the Theano computational graph
+h1_out = h1.fprop([x])
+y_hat = output.fprop([h1_out])
+
+# Compute the cost
+cost = NllMulInd(y, y_hat).mean()
+err = error(predict(y_hat), y)
+
+cost.name = 'cross_entropy'
 err.name = 'error_rate'
-model.graphs = [mlp]
+
+model.inputs = [x, y]
+model._params = params
+model.nodes = nodes
 
 # Define your optimizer: Momentum (Nesterov), RMSProp, Adam
 optimizer = RMSProp(
