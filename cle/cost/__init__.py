6450872b7f1b0827ac3837d4f3d61a672fd06779
Junyoung <elecegg@gmail.com>
2015-08-20 18:32:28 -0400

    updated

--- cle/cost/__init__.py
+++ cle/cost/__init__.py
@@ -13,9 +13,7 @@ def NllBin(y, y_hat):
     ----------
     .. todo::
     """
-    #nll = T.nnet.binary_crossentropy(y_hat, y).sum(axis=1)
-    ll = y * T.log(y_hat) + (1. - y) * T.log(1. - y_hat)
-    nll = -ll
+    nll = T.nnet.binary_crossentropy(y_hat, y).sum(axis=1)
     return nll
 
 
