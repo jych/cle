bfe9977a53de3c268bd6170fdfc41917196ad000
Junyoung <elecegg@gmail.com>
2015-09-07 15:41:51 -0400

    updated

--- cle/utils/op.py
+++ cle/utils/op.py
@@ -40,18 +40,18 @@ def add_noise_params(params, keys=['W'], std_dev=0.075):
     return nparams
 
 
-def Gaussian_sample(mu, sig, num_sample=None):
+def Gaussian_sample(mu, sig, num_sample=None, theano_rng=default_theano_rng):
 
     if num_sample is None:
         num_sample = 1
 
     mu = mu.dimshuffle(0, 'x', 1)
     sig = sig.dimshuffle(0, 'x', 1)
-    epsilon = self.theano_rng.normal(size=(mu.shape[0],
-                                           num_sample,
-                                           mu.shape[-1]),
-                                     avg=0., std=1.,
-                                     dtype=mu.dtype)
+    epsilon = theano_rng.normal(size=(mu.shape[0],
+                                      num_sample,
+                                      mu.shape[-1]),
+                                avg=0., std=1.,
+                                dtype=mu.dtype)
     z = mu + sig * epsilon
 
     if num_sample == 1:
