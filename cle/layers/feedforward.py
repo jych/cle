1a28587027f605a0435e40f94fd7b059a9e21718
Junyoung <elecegg@gmail.com>
2015-11-29 02:32:48 -0500

    added highway nets

--- cle/layers/feedforward.py
+++ cle/layers/feedforward.py
@@ -3,7 +3,7 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-from cle.cle.layers import StemCell
+from cle.cle.layers import StemCell, InitCell
 
 from itertools import izip
 
@@ -187,10 +187,10 @@ class VeryDeepFullyConnectedLayer(StemCell):
         if self.use_bias:
             params['b_'+self.name + '_h1'] = self.init_b.get(self.nout)
 
-        for i in xrange(1, self.num_layers):
+        for l in xrange(1, self.num_layers):
             W_shape = (self.nout, self.nout)
-            W_name = self.name + '_W_h%d__h%d' % (i, i+1)
-            b_name = self.name + '_b_h%d__h%d' % (i, i+1)
+            W_name = self.name + '_W_h%d__h%d' % (l, l+1)
+            b_name = self.name + '_b_h%d__h%d' % (l, l+1)
             params[W_name] = self.init_W.get(W_shape)
             params[b_name] = self.init_b.get(self.nout)
 
@@ -213,7 +213,7 @@ class VeryDeepFullyConnectedLayer(StemCell):
         z = T.zeros(z_shape, dtype=theano.config.floatX)
 
         for x, (parname, parout) in izip(X, self.parent.items()):
-            W_name = 'W_'+parname+'__'+self.name + '_h1'
+            W_name = 'W_' + parname + '__' + self.name + '_h1'
             W = tparams[W_name]
 
             if x.ndim == 1:
@@ -233,33 +233,158 @@ class VeryDeepFullyConnectedLayer(StemCell):
                     raise ValueError("your target ndim is less than the source ndim")
                 z += T.dot(x[:, :, :parout], W)
 
-        if not hasattr(self, 'use_bias'):
-            z += tparams['b_'+self.name+'_h1']
-        elif self.use_bias:
-            z += tparams['b_'+self.name+'_h1']
+        b_name = 'b_' + self.name + '_h1'
+        z += tparams[b_name]
 
         if z.ndim == 3:
-            z_shape = z.shape
-            z = self.nonlin(z.reshape((z_shape[0]*z_shape[1], -1))).reshape((z_shape[0], z_shape[1], -1))
-            z += self.cons
-        elif z.ndim == 2:
-            z = self.nonlin(z) + self.cons
+            z = z.reshape((z_shape[0]*z_shape[1], -1))
 
-        for i in xrange(1, self.num_layers):
-            inter_z = T.zeros(z_shape, dtype=theano.config.floatX)
+        z = self.nonlin(z)
 
-            W_name = self.name + '_W_h%d__h%d' % (i, i+1)
-            b_name = self.name + '_b_h%d__h%d' % (i, i+1)
+        for l in xrange(1, self.num_layers):
+
+            W_name = self.name + '_W_h%d__h%d' % (l, l+1)
+            b_name = self.name + '_b_h%d__h%d' % (l, l+1)
             W = tparams[W_name]
             b = tparams[b_name]
 
-            if z.ndim == 2:
-                inter_z = T.dot(z, W) + b
-                z = self.nonlin(inter_z) + self.cons
-            elif z.ndim == 3:
-                inter_z = T.dot(z, W) + b
-                z = self.nonlin(z.reshape((z_shape[0]*z_shape[1], -1))).reshape((z_shape[0], z_shape[1], -1))
-                z += self.cons
+            inter_z = T.dot(z, W) + b
+            z = self.nonlin(inter_z)
+
+        if z.ndim == 3:
+            z = z.reshape((z_shape[0], z_shape[1], -1))
+
+        z.name = self.name
+
+        return z
+
+
+class HighwayNet(StemCell):
+    """
+    Implementation of Highway Networks
+
+    Parameters
+    ----------
+    .. todo::
+    """
+    def __init__(self, num_layers,
+                 init_b_C=InitCell(init_type='const', mean=-3),
+                 **kwargs):
+        self.num_layers = num_layers
+        self.init_b_C = init_b_C
+        super(HighwayNet, self).__init__(**kwargs)
+
+    def initialize(self):
+
+        params = OrderedDict()
+
+        for parname, parout in self.parent.items():
+            W_shape = (parout, self.nout)
+            W_name = 'W_' + parname + '__' + self.name + '_h1'
+            params[W_name] = self.init_W.get(W_shape)
+            C_name = 'C_' + parname + '__' + self.name + '_h1'
+            params[C_name] = self.init_W.get(W_shape)
+
+        if self.use_bias:
+            b_name = 'b_' + self.name + '_h1'
+            params[b_name] = self.init_b.get(self.nout)
+            b_C_name = 'b_C_' + self.name + '_h1'
+            params[b_C_name] = self.init_b_C.get(self.nout)
+
+        for l in xrange(1, self.num_layers):
+            W_shape = (self.nout, self.nout)
+            W_name = self.name + '_W_h%d__h%d' % (l, l+1)
+            C_name = self.name + '_C_h%d__h%d' % (l, l+1)
+            b_name = self.name + '_b_h%d__h%d' % (l, l+1)
+            b_C_name = self.name + '_b_C_h%d__h%d' % (l, l+1)
+            params[W_name] = self.init_W.get(W_shape)
+            params[C_name] = self.init_W.get(W_shape)
+            params[b_name] = self.init_b.get(self.nout)
+            params[b_C_name] = self.init_b_C.get(self.nout)
+
+        return params
+
+    def fprop(self, X, tparams, ndim=None):
+
+        if len(X) != len(self.parent):
+            raise AttributeError("The number of inputs does not match "
+                                 "with the number of parents.")
+
+        # X could be a list of inputs.
+        # depending the number of parents.
+        if ndim is None:
+            ndims = [x.ndim for x in X]
+            idx = np.argmax(ndims)
+            ndim = np.maximum(np.array(ndims).max(), 2)
+
+        z_shape = [X[idx].shape[i] for i in xrange(ndim-1)] + [self.nout]
+        z = T.zeros(z_shape, dtype=theano.config.floatX)
+        z_C = T.zeros(z_shape, dtype=theano.config.floatX)
+
+        for x, (parname, parout) in izip(X, self.parent.items()):
+            W_name = 'W_' + parname + '__' + self.name + '_h1'
+            W = tparams[W_name]
+            C_name = 'C_' + parname + '__' + self.name + '_h1'
+            C = tparams[C_name]
+
+            if x.ndim == 1:
+                if 'int' not in x.dtype:
+                    x = T.cast(x, 'int64')
+                if z.ndim == 2:
+                    z += W[x]
+                    z_C += C[x]
+                elif z.ndim == 3:
+                    z += W[x][None, :, :]
+                    z_C += C[x][None, :, :]
+            elif x.ndim == 2:
+                if ndim == 2:
+                    z += T.dot(x[:, :parout], W)
+                    z_C += T.dot(x[:, :parout], C)
+                if ndim == 3:
+                    z += T.dot(x[:, :parout], W)[None, :, :]
+                    z_C += T.dot(x[:, :parout], C)[None, :, :]
+            elif x.ndim == 3:
+                if z.ndim != 3:
+                    raise ValueError("your target ndim is less than the source ndim")
+                z += T.dot(x[:, :, :parout], W)
+                z_C += T.dot(x[:, :, :parout], C)
+
+        b_name = 'b_' + self.name + '_h1'
+        b_C_name = 'b_C_' + self.name + '_h1'
+
+        z += tparams[b_name]
+        z_C += tparams[b_C_name]
+
+        z = self.nonlin(z.reshape((z_shape[0]*z_shape[1], -1))).reshape((z_shape[0], z_shape[1], -1))
+
+        if z.ndim == 3:
+            x = x.reshape((z_shape[0]*z_shape[1], -1))
+            z = z.reshape((z_shape[0]*z_shape[1], -1))
+            z_C = z_C.reshape((z_shape[0]*z_shape[1], -1))
+
+        z = self.nonlin(z)
+        z_C = T.nnet.sigmoid(z_C)
+        z = z * z_C + x * (1 - z_C)
+
+        for l in xrange(1, self.num_layers):
+
+            W_name = self.name + '_W_h%d__h%d' % (l, l+1)
+            C_name = self.name + '_C_h%d__h%d' % (l, l+1)
+            b_name = self.name + '_b_h%d__h%d' % (l, l+1)
+            b_C_name = self.name + '_b_C_h%d__h%d' % (l, l+1)
+            W = tparams[W_name]
+            C = tparams[C_name]
+            b = tparams[b_name]
+            b_C = tparams[b_C_name]
+
+            inter_z = T.dot(z, W) + b
+            inter_z_C = T.dot(z, C) + b_T
+            inter_z = self.nonlin(inter_z)
+            inter_z_C = T.nnet.sigmoid(inter_z_C)
+            z = inter_z * inter_z_C + z * (1 - inter_z_C)
+
+        if z.ndim == 3:
+            z = z.reshape((z_shape[0], z_shape[1], -1))
 
         z.name = self.name
 
